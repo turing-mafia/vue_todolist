f6c80897810600f25bd9d80392e59039f01a8cbb
mike2ox <firefinger07@gmail.com>
2019-11-05 23:04:00 +0900

    TODO functions
    - todo item
    - todo information
    - todo list

---
+++ src/components/Todoitem.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="todo-item" v-bind:class="{'is-complete':todo.completed}">
+        <p>
+            <input type="checkbox" v-on:change="markComplete">
+            {{todo.title}}
+            <button @click="$emit('del-todo', todo.id)" 
+            class="del"> X</button>
+            </p>
+    </div>
+</template>
+
+<script>
+export default {
+    name:"todoitem",
+    props:["todo"],
+    methods:{
+        markComplete(){
+            this.todo.completed = !this.todo.completed;
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .todo-item{
+        background: #f4f4f4;
+        padding:10px;
+        border-bottom: 1px
+    }
+
+    .is-complete{
+        text-decoration: line-through;
+    }
+
+    .del{
+        background: #ff0000
+    }
+</style>
